c57d99769e6b72051cf2d0e33964ad1da1e96b81
haruna147 <abubakarharunajunor@gmail.com>
2023-09-25 22:04:46 +0100

    loop

---
+++ views/assignment.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // <h2>
+    // loops are used to excute the same block of code a specified number of times in PHP.
+    // However, its of four types, which are;
+
+    // (1) while loop: the while loop excutes a block of code as long as the condition is true. example;
+
+
+    $x=3;
+    while(3<=90){
+        
+        print $x."<br/>";
+        $x++
+    }
+    // therefore, figure 3 will continue adding up till its equals 90, it will check d conditions again and break. 
+    // for info. about $x++ refer to oparators in php.
+
+
+    // (2) Do...while loop: the do while loop will always excute the block of code once, it will then check the condition
+        // and repeat while its true. example;
+
+        $x=9;
+        do{
+
+            print $x. <br/>;
+            $x++;
+        }while($x<7);
+
+        // from the example above, do while loop excute code at once. the 'whle' statement is false, therefore perfom once and break.
+
+
+    </h2>
+    
+    ?>
+</body>
+</html>
