9f0e372178ac50b4ec700147a1b0c8d3f58afa8d
haruna147 <abubakarharunajunor@gmail.com>
2023-10-02 21:39:46 +0100

    todo

--- activity.php
+++ activity.php
@@ -1,1 +1,29 @@
+<?php
 
+$_host = "127.0.0.1";
+$_password = "";
+$_username = "root";
+$_database = "login_system";
+
+$conn=new mysqli(
+    $_host,$_username,$_password,$_database
+);
+if($conn->connect_error) {
+    exit("error connecting");
+}
+print("connected..<br>");
+
+// exec query (SQL)
+// create
+$query = "INSERT INTO `users`( `name`, `password`) VALUES ('haeuna123','haruna@gmail.com')";
+$conn->query($query);
+
+
+
+
+
+
+
+
+
+?>
--- views/assignment.php
+++ views/assignment.php
@@ -14,12 +14,12 @@
     // (1) while loop: the while loop excutes a block of code as long as the condition is true. example;
 
 
-    $x=3;
-    while(3<=90){
+    // $x=3;
+    // while(3<=90){
         
-        print $x."<br/>";
-        $x++
-    }
+    //     print $x."<br/>";
+    //     $x++
+    // }
     // therefore, figure 3 will continue adding up till its equals 90, it will check d conditions again and break. 
     // for info. about $x++ refer to oparators in php.
 
@@ -27,17 +27,17 @@
     // (2) Do...while loop: the do while loop will always excute the block of code once, it will then check the condition
         // and repeat while its true. example;
 
-        $x=9;
-        do{
+        // $x=9;
+        // do{
 
-            print $x. <br/>;
-            $x++;
-        }while($x<7);
+        //     print $x. <br/>;
+        //     $x++;
+        // }while($x<7);
 
         // from the example above, do while loop excute code at once. the 'whle' statement is false, therefore perfom once and break.
 
 
-    </h2>
+    // </h2>
     
     ?>
 </body>
